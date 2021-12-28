6c168d727f6e7ba8b7b784e4bfba0b86d96e6fcd
Tom Barber <trb5160@gmail.com>
2021-12-27 20:19:04 -0500

    added intro messages for game

--- TripleXGame/tripleX.cpp
+++ TripleXGame/tripleX.cpp
@@ -1,4 +1,9 @@
+#include <iostream>
+
 int main()
 {
+    std::cout << "Jensen, I need you to hack this datapad to get into the server room...";
+    std::cout << std::endl;
+    std::cout << "You need to find the correct code...";
     return 0;
 }
